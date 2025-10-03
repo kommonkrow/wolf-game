6a767a59332b2924148c4d624cd79187ba955ff5
kommonkrow <w43t3rbr0g4@proton.me>
2025-10-03 16:25:43 -0500

    added player movement

---
+++ wolf-game/character_body_2d.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+@export var speed = 100
+
+var target = position
+
+func _input(_event):
+	look_at(get_global_mouse_position())
+	# Use is_action_pressed to only accept single taps as input instead of mouse drags.
+	if Input.is_action_pressed("rmb"):
+		target = get_global_mouse_position()
+	if Input.is_action_just_released("rmb"):
+		target = position
+		move_and_slide()
+
+func _physics_process(_delta):
+	velocity = position.direction_to(target) * speed * 10
+	# look_at(target)
+	if position.distance_to(target) > 10:
+		move_and_slide()
