467be2b6e86898ef10ea4c46fe5d093a4f5e1038
kommonkrow <w43t3rbr0g4@proton.me>
2025-10-23 14:40:20 -0500

    added hitboxes

---
+++ wolf-game/scripts/PreyCreature.gd
@@ -0,0 +1,15 @@
+extends Node
+
+#var predator = position
+
+#func DetectPredator():
+#Detect collision with Awareness by PlayerCreature
+
+#func FleeFromPredator():
+#Get location of predator 
+
+#func GoIdle():
+	
+
+#func IdleWander():
+	
