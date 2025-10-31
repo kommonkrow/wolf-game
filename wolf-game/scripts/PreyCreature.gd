f42167931fa7a8174a54c3e96737c94881c0671b
kommonkrow <w43t3rbr0g4@proton.me>
2025-10-31 13:26:18 -0500

    added follow camera

--- wolf-game/scripts/PreyCreature.gd
+++ wolf-game/scripts/PreyCreature.gd
@@ -1,62 +1,20 @@
 extends CharacterBody2D
 
-var preyHealth: int = 10
+var preyHealth: int = 3
 var maxHealth
 
-var activePredator = position
-signal isFleeing()
-var max_contacts_reported: int
-
-# signal set_max_contacts_reported(value: int)
-var contact_collision_pos: Vector2
-
-#--------- PREY MOVEMENT -----------#
-
-# ~~ IDLE MOVEMENT ~~ #
-
-func _physics_process(delta: float) -> void:
-	var collision = move_and_collide(velocity * delta)
-	if collision:
-		print("I collided with ", collision.get_collider().name)
-
-#func IdleWander():
-	#if isFleeing = false:
-		#pick random vector2
-		#move in direction of vector2 for X seconds
-		#idle in place for x seconds
-		#repeat
-
-
-#func _integrate_forces( state ):
-#	if(state.get_contact_count() >= 1):  #this check is needed or it will throw errors 
-#		contact_collision_pos = state.get_contact_collider_pos()
-
-# ~~ FLEEING ~~ #
 
 func _on_awareness__elk_area_entered(area: Area2D) -> void:
-	#Detect collision with Awareness by PlayerCreature
 	if area.is_in_group("Carnivore"):
-		var collision_position = contact_collision_pos + get_position_delta()
-		print("Contact collision: %s" % contact_collision_pos)
-#		area.global_position = activePredator
-#		print("activePredator = %s" % activePredator)
-		isFleeing.emit(true)
+		print("Carnivore detected.")
+
+#func flee_predator():
+	#direction = (predator.position + position).normalized()
+	#position += direction * speed
+	##switch to Timer timeout!!!!!!!!
+	#if position.distance_to(predator.position) > 100:
+		#current_state = State.WANDER
 		
-
-func FleeFromPredator(activePredator):
-	#check isFleeing
-	if isFleeing:
-		print("Running isFleeing from predator")
-		
-	print("activePredator = %s" % activePredator)
-#Get location of predator 
-
-#Move away from predator
-
-#func GoIdle():
-	
-
-#--------- PREY BEHAVIOR -----------#
 
 # ~~ TAKE DAMAGE ~~ #
 func _on_hit_box__elk_whole_body_area_entered(area: Area2D):
